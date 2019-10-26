b461d5757f211633767bc12249035679918bc708
Wagner Xu <wagnerhsu@hotmail.com>
2019-10-26 22:07:34 +0800

    Add accordion

---
+++ MyCode/Chap06/Chap06/wwwroot/js/Index.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+    $("div").on("click",
+        "p",
+        function() {
+            console.log("Hello World");
+        });
+    $("<p />",
+        {
+            text: "Paragraph 6"
+        }).appendTo("div");
+});
