9bc618790928dae77657a46811462dbe5b9619ad
Wagner Xu <wagnerhsu@hotmail.com>
2019-10-27 11:19:06 +0800

    Add files

---
+++ MyCode/Chap08/AjaxDemo/wwwroot/js/AjaxTest/Index.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $("#ajaxLocalJson").on("click",
+        function () {
+            $.ajax({
+                "url": "/sample.json",
+                "type": "get",
+                "dataType": "json"
+            }).done(function (results) {
+                console.log(results);
+            });
+        });
+});
