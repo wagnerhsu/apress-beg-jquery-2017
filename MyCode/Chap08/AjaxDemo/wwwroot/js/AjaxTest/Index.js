48771cde4d0aead2d5927e5e90e3b9d0cde99271
Wagner Xu <wagnerhsu@hotmail.com>
2019-11-23 20:42:16 +0800

    Update files

--- MyCode/Chap08/AjaxDemo/wwwroot/js/AjaxTest/Index.js
+++ MyCode/Chap08/AjaxDemo/wwwroot/js/AjaxTest/Index.js
@@ -7,6 +7,26 @@
                 "dataType": "json"
             }).done(function (results) {
                 console.log(results);
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            });
+        });
+    $("#ajaxLocalJson").on("click",
+        function () {
+            $.getJSON("/sample.json",
+                function(data) {
+                    console.log(data);
+                });
+        });
+    $("#ajaxWithError").on("click",
+        function() {
+            $.ajax(
+                {
+                    "url": "sample.json",
+                    "type": "get",
+                    "dataType":"json"
+                }).fail(function() {
+                console.log("fail", arguments);
             });
         });
 });
